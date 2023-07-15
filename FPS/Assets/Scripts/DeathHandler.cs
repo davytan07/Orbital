a74948a106a06cd821d3a54a1459e01f008b587c
davytan07 <45758052+davytan07@users.noreply.github.com>
2023-07-15 17:08:46 +0800

    Added Current Score and Highscore display to Game Over Canvas
    - Applied to the 3 maps

--- FPS/Assets/Scripts/DeathHandler.cs
+++ FPS/Assets/Scripts/DeathHandler.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DeathHandler : MonoBehaviour
 {
     [SerializeField] Canvas gameOverCanvas;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI highScoreText;
     public AudioSource DeathSFX;
+    PointsSystem ps;
     PlayerMotor motor;
 
     private void Start()
     {
         gameOverCanvas.enabled = false;
         motor = GetComponent<PlayerMotor>();
+        ps = GetComponent<PointsSystem>();
     }
 
     public void HandleDeath()
@@ -23,5 +29,7 @@ public class DeathHandler : MonoBehaviour
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        scoreText.text = $"SCORE:  {ps.CurrentPoints().ToString()}";
+        highScoreText.text = $"HIGHSCORE:  {PlayerPrefs.GetInt("HighScore", 0)}";
     }
 }
